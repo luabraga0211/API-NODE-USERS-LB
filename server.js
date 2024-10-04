b0901b4e4e30a343682e62827be6493ab66ca8e3
luabraga0211 <141754027+luabraga0211@users.noreply.github.com>
2024-10-04 12:28:38 -0300

    primeiro commit
    add

---
+++ server.js
@@ -0,0 +1,67 @@
+
+import { fastify } from 'fastify'
+import cors from '@fastify/cors'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify();
+const databasePostgres = new DatabasePostgres;
+
+// CORS
+server.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+})
+
+// ENDPOINTS (CRUD):
+
+// CREATE
+server.post('/users', async (request, reply) => {
+    const body = request.body;
+    let error = {};
+    if (!body.name) {
+        error.name = 'faltou o nome do usuario'
+    }
+    if (!body.password) {
+        error.password = 'Senha ausente, crie uma'
+    }
+    if (!body.profile) {
+        error.profile = 'Perfil ausente'
+    }
+
+    if (body.name && body.password && body.profile) {
+        await databasePostgres.createUser(body);
+        return reply.status(201).send('A criação foi bem sucedida');
+    }
+    else {
+        return reply.status(400).send(error);
+    }
+
+})
+
+// READE
+server.get('/users', async () => {
+    const users = await databasePostgres.listUsers();
+    return users;
+});
+
+// UPDATE
+server.put('/users/:id', async (request, reply) => {
+    const userID = request.params.id;
+    const body = request.body;
+    await databasePostgres.updateUser(userID, body);
+
+    return reply.status(204).send();
+})
+
+// DELETE
+server.delete('/users/:id', async (request, reply) => {
+    const userID = request.params.id;
+    await databasePostgres.deleteUser(userID);
+
+    return reply.status(204).send();
+})
+
+
+server.listen({
+    port: 3333
+});
